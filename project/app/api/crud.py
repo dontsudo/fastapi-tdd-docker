0a89eed73828f6c9cf50923f2767b0ec9cce1acf
Reeul <chotnt741@gmail.com>
2022-10-21 18:43:10 +0900

    chore

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,11 +1,11 @@
 from typing import List, Union
 
-from app.models.pydantic import SummaryPayloadSchema, SummaryUpdatePayloadSchema
+from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
-    summary = TextSummary(url=payload.url, summary="dummy summary")
+    summary = TextSummary(url=payload.url, summary="")
     await summary.save()
     return summary.id
 
@@ -27,7 +27,7 @@ async def delete(id: int) -> int:
     return summary
 
 
-async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Union[dict, None]:
+async def put(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
     summary = await TextSummary.filter(id=id).update(
         url=payload.url, summary=payload.summary
     )
